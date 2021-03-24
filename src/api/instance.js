495cfd3e3ec94af2e551764900c53a1414070d53
Grin <asusgrin@gmail.com>
2021-03-24 17:13:31 +0200

    Update instance.js

--- src/api/instance.js
+++ src/api/instance.js
@@ -21,7 +21,7 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: 'https://apiv2.golden.net.ua/v2',
     headers: {
-        "X-Auth-Key": "0cc5e994-47ce-47c8-af25-f8cd4006d90a",
+        "X-Auth-Key": key,
         accept: "application/json"
     }
 })
